91d55b42b4e41badcab7bcd20b4092ec425f3846
TheWolfA2 <man_of_magik903@yahoo.com>
2016-05-18 18:20:37 -0700

    Add project files.

---
+++ Selama/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Selama.MvcApplication" Language="C#" %>
---
+++ Selama/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Selama.Startup))]
+namespace Selama
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
