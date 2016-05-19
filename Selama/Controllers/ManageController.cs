16165f491fc160b05949942ae0080b709e04ccff
TheWolfA2 <man_of_magik903@yahoo.com>
2016-05-18 19:09:00 -0700

    Namespaced ManageViewModels to ViewModels.Manage

--- Selama/Controllers/ManageController.cs
+++ Selama/Controllers/ManageController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Selama.Models;
+using Selama.ViewModels.Manage;
 
 namespace Selama.Controllers
 {
