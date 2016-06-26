0c1b238e3e2686c1ca7d11bd6f7d91d18f0e3100
TheWolfA2 <man_of_magik903@yahoo.com>
2016-06-25 19:23:30 -0700

    Started adding some unit tests

---
+++ Selama.Tests/Areas/Forum/Controllers/ForumControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Selama.Areas.Forums.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Selama.Tests.Areas.Forum.Controllers
+{
+    [TestClass]
+    public class ForumControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            ForumController controller = new ForumController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ThreadsNoId()
+        {
+            ForumController controller = new ForumController();
+
+            RedirectToRouteResult result = controller.Threads() as RedirectToRouteResult;
+
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void ThreadsValidId()
+        {
+            ForumController controller = new ForumController();
+
+            ViewResult result = controller.Threads(1) as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
