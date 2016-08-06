2bca76671f6242d73911018ef7926322d7a3b9db
TheWolfA2 <wolfington98@gmail.com>
2016-08-06 10:16:42 -0700

    Moved javascript from Threads.cshtml to a JS file

--- Selama/Areas/Forums/Scripts/Forums.js
+++ Selama/Areas/Forums/Scripts/Forums.js
@@ -12,6 +12,21 @@ Selama.Forums = Selama.Forums || {
     },
 };
 
+Selama.Forums.Threads = Selama.Forums.Threads || {
+    onThreadTitleMouseMove: function Selama_Forums_Threads_OnThreadTitleMouseMove(e)
+    {
+        /// <param name="e" type="jQuery.Event" />
+        $(e.target).popover("show");
+        $(".popover").css({ top: e.pageY - 14, left: e.pageX + 6 }).find(".arrow").css("top", "14px");
+    },
+
+    onThreadTitleMouseLeave: function Selama_Forums_Threads_OnThreadTitleMouseLeave(e)
+    {
+        /// <param name="e" type="jQuery.Event" />
+        $(e.target).popover("hide");
+    },
+};
+
 Selama.Forums.Thread = Selama.Forums.Thread || {
     onPostReplyClick: function onPostReplyClick()
     {
