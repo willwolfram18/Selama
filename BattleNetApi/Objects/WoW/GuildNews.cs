2da52270bb221ac2e8fc88ff733eba537177f3b3
TheWolfA2 <man_of_magik903@yahoo.com>
2016-08-29 19:11:13 -0700

    Started guildnews object

---
+++ BattleNetApi/Objects/WoW/GuildNews.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleNetApi.Objects.WoW
+{
+    public class GuildNews
+    {
+        public string Type { get; private set; }
+
+        public string CharacterName { get; private set; }
+
+        public DateTime Timestamp { get; private set; }
+
+        public int? ItemId { get; private set; }
+
+        public string Context { get; private set; }
+
+        public List<int> BonusLists { get; private set; }
+
+        public Achievement Achievement { get; private set; }
+    }
+}
