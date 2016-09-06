278f54c29f0179bb187bc07e712f98dbbd0a2733
TheWolfA2 <man_of_magik903@yahoo.com>
2016-09-05 17:13:52 -0700

    Started guild news panel on home page

--- Selama/Controllers/__BaseController.cs
+++ Selama/Controllers/__BaseController.cs
@@ -15,6 +15,67 @@ namespace Selama.Controllers
     {
         public const int HTTP_UNPROCESSABLE_ENTITY = 422;
 
+        protected bool TrySaveChanges(DbContext db)
+        {
+            try
+            {
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        protected async Task<bool> TrySaveChangesAsync(DbContext db)
+        {
+            try
+            {
+                await db.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateConcurrencyException concurrencyException)
+            {
+                ModelState.AddModelError("", "The data was updated before your changes could be saved.");
+                foreach (var entity in concurrencyException.Entries)
+                {
+                    await entity.ReloadAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return false;
+        }
+
+        protected bool TrySaveChanges(DbContext db, out SaveChangeError result)
+        {
+            try
+            {
+                db.SaveChanges();
+                result = SaveChangeError.None;
+                return true;
+            }
+            catch (Exception e)
+            {
+                if (e is DbUpdateConcurrencyException)
+                {
+                    result = SaveChangeError.ConcurrencyError;
+                    DbUpdateConcurrencyException concurrentError = e as DbUpdateConcurrencyException;
+                    foreach (var entity in concurrentError.Entries)
+                    {
+                        entity.Reload();
+                    }
+                }
+                else
+                {
+                    result = SaveChangeError.Unknown;
+                }
+                return false;
+            }
+        }
+
         protected void CompareProperties<TIn>(string propertyName, TIn dbValue, TIn modelValue, Func<TIn, TIn, bool> areEqualComp)
         {
             CompareProperties<TIn>(propertyName, dbValue, modelValue, "{0}", areEqualComp);
