263d2f0d80814757cc4495b7d746c4afc277462e
TheWolfA2 <wolfington98@gmail.com>
2016-09-15 12:08:57 -0700

    started html extension methods class; started using open graph protocol

---
+++ Selama/Common/ExtensionMethods/HtmlHelperExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Selama.Common.ExtensionMethods
+{
+    public static class HtmlHelperExtensionMethods
+    {
+        public static MvcHtmlString OpenGraphProtocolMetaTag(this HtmlHelper helper, string propertyName, string content)
+        {
+            return new MvcHtmlString(string.Format("<meta property='og:{0}' content='{1}' />", propertyName, content));
+        }
+    }
+}
