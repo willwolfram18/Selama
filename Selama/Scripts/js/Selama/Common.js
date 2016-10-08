3974c83939ca4916941eca690a2aeb5390aefab5
TheWolfA2 <man_of_magik903@yahoo.com>
2016-10-08 14:17:45 -0700

    Trying to move BACK from TypeScript to plain JS

---
+++ Selama/Scripts/js/Selama/Common.js
@@ -0,0 +1,30 @@
+﻿var Selama = Selama || {};
+
+Selama.Core = {};
+Selama.Core.$$bind = function Selama_Core_$$bind(func, context)
+{
+    /// <param name="func" type="Function" />
+    /// <param name="context" type="Object" />
+    return func.bind(context);
+};
+
+Selama.Core.createElem = function Selama_Core_CreateElement(tagName, cssClass, id)
+{
+    /// <param name="tagName" type="String" />
+    /// <param name="cssClass" type="String" />
+    /// <param name="id" type="String" />
+    /// <returns type="jQuery" />
+    return $("<" + tagName + "/>").addClass(cssClass).attr("id", id);
+};
+
+Selama.Core.generateFixedTable = function Selama_Core_GenerateFixedTable()
+{
+    $(".table.table-fixed-col.active").remove();
+    var $table = $(".table.table-fixed-col");
+    var $fixedTable = $table.clone().insertBefore($(".table")).addClass("active");
+    $fixedTable.find("th:not(:first-child),td:not(:first-child)").remove();
+    $fixedTable.find("tr").each(function (i, elem)
+    {
+        $(this).height($table.find("tr:eq(" + i + ")").height());
+    });
+};
---
+++ Selama/Scripts/js/_references.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="~/Scripts/js/jQuery/jquery-3.1.0.intellisense.js" />
+/// <reference path="~/Scripts/js/jQuery/jquery-3.1.0.js" />
+/// <reference path="~/Scripts/js/Bootstrap/bootstrap.js" />
+/// <reference path="~/Scripts/js/MarkdownDeep/MarkdownDeepLib.min.js" />
+/// <reference path="~/Scripts/js/Selama.js" />
